e6da1f389a6673c95df49731d038287ec45f1f12
Wayne Graham <wayne.graham@virginia.edu>
2015-05-04 15:45:09 -0400

    Updates all the things

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 popular [Mapbox](https://www.mapbox.com/) platform. However, it requires
 building a special text file in a really weird format that can make your
 eyes bleed if you are not a Cylon. This tool helps.
+
+http://mapwarper.net/layers/wms/348?SERVICE=WMS&VERSION=1.1.1&SLD_VERSION=1.1.0&REQUEST=GetLegendGraphic&FORMAT=image/png&LAYER=image&STYLE=
+
+http://mapwarper.net/layers/wms/348?request=GetCapabilities&service=WMS&version=1.1.1
--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,31 @@
 /* global describe, it */
 
+var should = require('should'),
+  assert = require('assert'),
+  jsdom = require('jsdom');
+
+var M = require('../app/scripts/main.js');
+
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  describe('Mapbox generator', function() {
+    // create a dom so jquery will work
+    var doc = jsdom.jsdom(html),
+      window = doc.parentWindow,
+      $ = global.jQuery = require('jquery')(window);
 
+      it('generates json when you submit the form', function() {
+        $('form').submit();
+        $('div#json-data').length.should.eq(1);
       });
-    });
   });
+
+  //describe('Give it some context', function () {
+    //describe('maybe a bit more context here', function () {
+      //it('should run here few assertions', function () {
+
+      //});
+    //});
+  //});
 })();
